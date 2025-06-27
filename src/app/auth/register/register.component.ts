f85e41abf28d25ddc6559c29c6bec6e71f4835c0
nicolas <aguilar-nicolas@javeriana.edu.co>
2025-06-27 10:21:50 -0500

    changes in register component and changes in login

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -3,42 +3,63 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   standalone: true,
   selector: 'app-register',
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatProgressSpinnerModule
+  ],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
   registerForm: FormGroup;
   mostrarContrasena = false;
+  isLoading = false;
 
-  constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) {
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
     this.registerForm = this.fb.group({
       nombre: ['', Validators.required],
       correo: ['', [Validators.required, Validators.email]],
       contrasena: ['', Validators.required]
     });
   }
-   irARegistro(): void {
-    this.router.navigate(['/login']);
+
+  toggleMostrar(): void {
+    this.mostrarContrasena = !this.mostrarContrasena;
   }
 
-
-  toggleMostrar() {
-    this.mostrarContrasena = !this.mostrarContrasena;
+  irAInicioSesion(): void {
+    this.router.navigate(['/login']);
   }
 
   onSubmit(): void {
     if (this.registerForm.valid) {
+      this.isLoading = true;
       this.authService.register(this.registerForm.value).subscribe({
         next: () => {
+          this.isLoading = false;
           alert('✅ Registro exitoso');
           this.router.navigate(['/login']);
         },
         error: () => {
+          this.isLoading = false;
           alert('❌ Error en el registro');
         }
       });
